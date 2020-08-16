a39b587fc0242f2e75af710a5c5a5e0d03bd0643
skratchdot <git@skratchdot.com>
2020-08-15 20:37:13 -0400

    Updates

---
+++ precache-manifest.910cad36ae98d9e984fde16e2c480f63.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b08fbafb46207d0682bfcff7264db1af",
+    "url": "/web-audio-api-v2-issue-7/index.html"
+  },
+  {
+    "revision": "d4cc8a3fab0eb6cee773",
+    "url": "/web-audio-api-v2-issue-7/static/css/main.585816ac.chunk.css"
+  },
+  {
+    "revision": "30d36f36b14f90570b42",
+    "url": "/web-audio-api-v2-issue-7/static/js/2.1e7c8ae9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/web-audio-api-v2-issue-7/static/js/2.1e7c8ae9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4cc8a3fab0eb6cee773",
+    "url": "/web-audio-api-v2-issue-7/static/js/main.3195db9a.chunk.js"
+  },
+  {
+    "revision": "1866235423bddbdf2f32",
+    "url": "/web-audio-api-v2-issue-7/static/js/runtime-main.60da718f.js"
+  }
+]);
---
+++ pulse-oscillator.js
@@ -0,0 +1,85 @@
+/* globals currentFrame, currentTime, sampleRate, registerProcessor */
+
+// a few default values
+const DEFAULT_FREQUENCY = 440;
+const DEFAULT_DETUNE = 0;
+const DEFAULT_PULSE_WIDTH = 0.5;
+
+/**
+ * helper function for getting param values
+ *
+ * @param {*} param
+ */
+const paramGetter = (param) =>
+  param.length > 1 ? (n) => param[n] : () => param[0];
+
+/**
+ * A Pulse Oscillator with a pulseWidth audioParam.  It should behave
+ * very similar to the "square" wave oscillator with the caveat that
+ * pulseWidth can be set.
+ *
+ * https://github.com/WebAudio/web-audio-api-v2/issues/7
+ *
+ * @class PulseOscillator
+ * @extends AudioWorkletProcessor
+ */
+class PulseOscillator extends AudioWorkletProcessor {
+  static get parameterDescriptors() {
+    return [
+      {
+        name: 'frequency',
+        defaultValue: DEFAULT_FREQUENCY,
+        minValue: -0.5 * sampleRate,
+        maxValue: 0.5 * sampleRate,
+        automationRate: 'a-rate',
+      },
+      {
+        name: 'detune',
+        defaultValue: DEFAULT_DETUNE,
+        minValue: -153600,
+        maxValue: 153600,
+        automationRate: 'a-rate',
+      },
+      {
+        name: 'pulseWidth',
+        defaultValue: DEFAULT_PULSE_WIDTH,
+        minValue: 0,
+        maxValue: 1,
+        automationRate: 'a-rate',
+      },
+    ];
+  }
+
+  constructor() {
+    super();
+    this.pos = 0;
+    this.minPhase = -1;
+    this.maxPhase = 1;
+  }
+
+  process(inputs, outputs, parameters) {
+    const output = outputs[0];
+    const getFrequency = paramGetter(parameters.frequency);
+    const getDetune = paramGetter(parameters.detune);
+    const getPulseWidth = paramGetter(parameters.pulseWidth);
+
+    output.forEach((channel) => {
+      for (let i = 0; i < channel.length; i++) {
+        const frequency = getFrequency(i);
+        const detune = getDetune(i);
+        const pulseWidth = getPulseWidth(i);
+        const freq = frequency * Math.pow(2, detune / 1200);
+        const r = this.pos % 1; // get remainder (a value between 0 and 1)
+        const phase = r < 0 ? 0 - r : r; // handle negative frequencies
+        // set output value
+        channel[i] = phase < pulseWidth ? this.minPhase : this.maxPhase;
+        // increase phase
+        this.pos += freq / sampleRate;
+      }
+    });
+
+    return true;
+  }
+}
+
+registerProcessor('pulse-oscillator', PulseOscillator);
