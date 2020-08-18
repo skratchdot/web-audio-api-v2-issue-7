541b6c53febe49d640e9b965284864b8bfeb4ac2
skratchdot <git@skratchdot.com>
2020-08-17 23:16:17 -0400

    Updates

--- pulse-oscillator.js
+++ pulse-oscillator.js
@@ -1,32 +1,26 @@
 /* globals currentFrame, currentTime, sampleRate, registerProcessor */
-/**
- * polyblep code taken from "mystran" on the forum:
- * https://www.kvraudio.com/forum/viewtopic.php?f=33&t=398553
- */
 
 // a few default values
 const DEFAULT_FREQUENCY = 440;
 const DEFAULT_DETUNE = 0;
 const DEFAULT_PULSE_WIDTH = 0.5;
-const NO_PWM_FIX = false;
-
-// The first sample BLEP
-const poly3blep0 = (t) => {
-  // these are just sanity checks
-  // correct code doesn't need them
-  if (t < 0) return 0;
-  if (t > 1) return 1;
-
-  const t2 = t * t;
-  return t * t2 - 0.5 * t2 * t2;
-};
-
-// And second sample as wrapper, optimize if you want.
-const poly3blep1 = (t) => -poly3blep0(1 - t);
+const TWOPI = Math.PI * 2;
 
 /**
- * helper function for getting param values
- *
+ * enforce: -TWOPI < phase < TWOPI
+ * @param {*} phase 
+ */
+const adjustPhase = (phase) => {
+  if (phase >= Math.PI) {
+    phase -= TWOPI;
+  } else if (phase < -Math.PI) {
+    phase += TWOPI;
+  }
+  return phase;
+};
+
+/**
+ * helper function for getting audio param values. we either have 1 or 128
  * @param {*} param
  */
 const paramGetter = (param) =>
@@ -71,134 +65,43 @@ class PulseOscillator extends AudioWorkletProcessor {
 
   constructor() {
     super();
-    this.reset();
-  }
-  reset() {
-    // set current phase to 0
+    this.twoPiOverSr = TWOPI / sampleRate;
+    this.freq = 0;
     this.phase = 0;
-    // blep delay "buffer" to zero
-    this.blepDelay = 0;
-    // previous pulseWidth to any valid value
-    this.widthDelay = 0.5;
-    // only 2 stages here, set to first
-    this.pulseStage = 0;
+    this.incr = 0;
   }
   process(inputs, outputs, parameters) {
     const output = outputs[0];
     const getFrequency = paramGetter(parameters.frequency);
     const getDetune = paramGetter(parameters.detune);
     const getPulseWidth = paramGetter(parameters.pulseWidth);
-    const mix = 1; // 0=saw, 1=pulse
 
     output.forEach((channel) => {
-      const nsamples = channel.length;
-
-      for (let i = 0; i < nsamples; ++i) {
-        // calculate frequency
+      for (let i = 0; i < channel.length; i++) {
+        // get our current param values
         const frequency = getFrequency(i);
         const detune = getDetune(i);
-        const calculatedFreq = frequency * Math.pow(2, detune / 1200);
-        // normalized frequency
-        const freq = Math.abs(calculatedFreq / sampleRate);
+        const pulseWidth = getPulseWidth(i);
+        // calculate frequency
+        const freq = frequency * Math.pow(2, detune / 1200);
 
-        // the BLEP latency is 1 sample, so first
-        // take the delayed part from previous sample
-        let out = this.blepDelay;
+        // channel[i] = Math.sin(this.phase); // sine
+        // channel[i] = this.phase / Math.PI; // sawtooth
+        // channel[i] = this.phase <= TWOPI * pulseWidth - Math.PI ? 1 : -1; // square
 
-        // then reset the delay so we can build into it
-        this.blepDelay = 0;
+        // set carrier phase based on current phase and pulseWidth
+        const carrierPhase = adjustPhase(this.phase + TWOPI * pulseWidth);
+        const sawtooth1 = this.phase / Math.PI;
+        const sawtooth2 = carrierPhase / Math.PI;
+        channel[i] = sawtooth2 - sawtooth1;
 
-        // then proceed like a trivial oscillator
-        this.phase += freq;
-
-        // load and sanity-check the new PWM
-        // ugly things would happen outside range
-        let pulseWidth = getPulseWidth(i);
-        if (pulseWidth > 1) pulseWidth = 1;
-        if (pulseWidth < 0) pulseWidth = 0;
-
-        // Then replace the reset logic: loop until we
-        // can't find the next discontinuity, so during
-        // one sample we can process any number of them!
-        while (true) {
-          let t;
-          // Now in order of the stages of the wave-form
-          // check for discontinuity during this sample.
-
-          // First is the "pulse-width" transition.
-          if (this.pulseStage === 0) {
-            // if we didn't hit the pulse-width yet
-            // break out of the while(true) loop
-            if (this.phase < pulseWidth) break;
-
-            if (NO_PWM_FIX) {
-              // otherwise solve transition: when during
-              // this sample did we hit the pw-border..
-              // t = (1-x) from: phase + (x-1)*freq = pw
-              t = (this.phase - pulseWidth) / freq;
-            } else {
-              // the above version is fine when pw is constant
-              // and it's what we use for the reset part, but
-              // for pw modulation, t could end up outside [0,1]
-              // and that will sound pretty ugly, so use lerp:
-              //   phase + (x-1)*freq = (1-x)*pwOld + x*pw
-              // and again t = (1 - x), and hopefully..
-              t =
-                (this.phase - pulseWidth) /
-                (this.widthDelay - pulseWidth + freq);
-            }
-            // so then scale by pulse mix
-            // and add the first sample to output
-            out += mix * poly3blep0(t);
-
-            // and second sample to delay
-            this.blepDelay += mix * poly3blep1(t);
-
-            // and then we proceed to next stage
-            this.pulseStage = 1;
-          }
-
-          // then whether or not we did transition, if stage
-          // is at this point 1, we process the final reset
-          if (this.pulseStage === 1) {
-            // not ready to reset yet?
-            if (this.phase < 1) break;
-
-            // otherwise same as the pw, except threshold 1
-            t = (this.phase - 1) / freq;
-
-            // and negative transition.. normally you would
-            // calculate step-sizes for all mixed waves, but
-            // here both saw and pulse go from 1 to 0.. so
-            // it's always the same transition size!
-            out -= poly3blep0(t);
-            this.blepDelay -= poly3blep1(t);
-
-            // and then we do a reset (just one!)
-            this.pulseStage = 0;
-            this.phase -= 1;
-          }
-
-          // and if we are here, then there are possibly
-          // more transitions to process, so keep going
+        // set new phase
+        if (this.freq !== freq) {
+          this.freq = freq;
+          this.incr = this.twoPiOverSr * freq;
         }
-
-        // When the loop breaks (and it'll always break)
-        // we have collected all the various BLEPs into our
-        // output and delay buffer, so add the trivial wave
-        // into the buffer, so it's properly delayed
-        //
-        // note: using pulseStage instead of pw-comparison
-        // avoids inconsistencies from numerical inaccuracy
-        this.blepDelay +=
-          (1 - mix) * this.phase + mix * (this.pulseStage ? 1 : 0);
-
-        // and output is just what we collected, but
-        // let's make it range -1 to 1 instead
-        channel[i] += 2 * out - 1;
-
-        // and store pulse-width delay for the lerp
-        this.widthDelay = pulseWidth;
+        this.phase += this.incr;
+        this.phase = adjustPhase(this.phase);
       }
     });
 
